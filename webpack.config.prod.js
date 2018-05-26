7739e224e577e8296a45fe8ed130852bc35f4872
mastermindzh <info@rickvanlieshout.com>
2018-05-26 18:41:13 +0200

    github pages prep

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ export default {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
